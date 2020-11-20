c9ea9cc89029914956165572a4052de70d33319c
MCAST-GeorgeSammut <49560805+MCAST-GeorgeSammut@users.noreply.github.com>
2020-11-20 12:35:11 +0100

    made three changes

---
+++ Resources/Scripts/UIScripts/scoreManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scoreManager : MonoBehaviour
+{
+    Text scoreText;
+
+    public static int scoreValue =0;
+
+    public bool scoreStarted;
+
+
+
+    IEnumerator scoreTracker()
+    {
+        while (true)
+        {
+            if (scoreStarted)
+            {
+                scoreText.text = "Score: " + scoreValue;
+
+                yield return null;
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        StartCoroutine(scoreTracker());
+    }
+
+}
---
+++ Resources/Scripts/snakeheadController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class snakeheadController : MonoBehaviour
+{
+    snakeGenerator mysnakegenerator;
+    foodGenerator myfoodgenerator;
+
+    private void Start()
+    {
+        mysnakegenerator = Camera.main.GetComponent<snakeGenerator>();
+        myfoodgenerator = Camera.main.GetComponent<foodGenerator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position -= new Vector3(1f,0);
+            myfoodgenerator.eatFood(this.transform.position);
+            Debug.Log(mysnakegenerator.hitTail(this.transform.position, mysnakegenerator.snakelength));
+
+            if (mysnakegenerator.hitTail(this.transform.position, mysnakegenerator.snakelength))
+            {
+                SceneManager.LoadScene("endScene");
+            }
+
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1f, 0);
+            myfoodgenerator.eatFood(this.transform.position);
+            Debug.Log(mysnakegenerator.hitTail(this.transform.position, mysnakegenerator.snakelength));
+
+            if (mysnakegenerator.hitTail(this.transform.position, mysnakegenerator.snakelength))
+            {
+                SceneManager.LoadScene("endScene");
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0, 1f);
+            myfoodgenerator.eatFood(this.transform.position);
+            Debug.Log(mysnakegenerator.hitTail(this.transform.position, mysnakegenerator.snakelength));
+
+            if (mysnakegenerator.hitTail(this.transform.position, mysnakegenerator.snakelength))
+            {
+                SceneManager.LoadScene("endScene");
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.position -= new Vector3(0, 1f);
+            myfoodgenerator.eatFood(this.transform.position);
+            Debug.Log(mysnakegenerator.hitTail(this.transform.position, mysnakegenerator.snakelength));
+
+            if (mysnakegenerator.hitTail(this.transform.position, mysnakegenerator.snakelength))
+            {
+                SceneManager.LoadScene("endScene");
+            }
+        }
+
+        //Debug.Log(mysnakegenerator.hitTail(this.transform.position, mysnakegenerator.snakelength)); 
+    }
+}
